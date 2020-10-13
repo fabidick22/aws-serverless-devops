854a305d71f0d47e7bb8c96fb24646b5b684660f
Dickson Armijos <fabidick22@hotmail.es>
2020-10-13 17:21:05 -0500

    fix: update unit test

--- src/handlers/save-data/tests/unit/test-handler.js
+++ src/handlers/save-data/tests/unit/test-handler.js
@@ -16,7 +16,7 @@ describe('Tests index', function () {
         let response = JSON.parse(result.body);
 
         expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
+        expect(response.message).to.be.equal("save data");
         // expect(response.location).to.be.an("string");
     });
 });
