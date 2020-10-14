97eafc44f9fbf92b8d4d0d21581291f1b77c8132
Dickson Armijos <fabidick22@hotmail.es>
2020-10-14 14:45:22 -0500

    feat: Add logs

--- src/handlers/get-data/app.js
+++ src/handlers/get-data/app.js
@@ -2,12 +2,13 @@
 let response;
 
 exports.lambdaHandler = async (event, context) => {
+    const body = JSON.parse(event.body)
+    console.info(JSON.parse(context))
     try {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
                 message: 'get data',
-                // location: ret.data.trim()
             })
         }
     } catch (err) {
